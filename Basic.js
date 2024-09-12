2c88b1406b980ac20dd1f1e3009a6ba11ab6b0da
khan je@BilalShinwari <bilal96shinwari@gmail.com>
2024-09-12 20:43:46 +0500

    little basics and create object

---
+++ Basic.js
@@ -0,0 +1,19 @@
+// Object
+
+const data = {
+    name: "Bilal",
+    Fname: "Muhammad Ali",
+    Rollno: "7",
+    semester: "7th",
+    Discipline: "Computer Science",
+    GPA: 3.5,
+    inpass: true,
+    Address:{
+        city: "landikotal",
+        District: "khyber",
+        Village: "Alam Khani"
+    }
+}
+console.log(data.name);
+console.log(data.Address.city)
+console.log(data.GPA)
